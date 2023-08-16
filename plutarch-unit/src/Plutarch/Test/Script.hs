5ca65d93e4000beb65b526ccbd1cdc9bb61c32e4
Peter Dragos <peter@mlabs.city>
2023-08-16 16:04:41 -0400

    testing to see if the regular script result needs to be huge, too

--- plutarch-unit/src/Plutarch/Test/Script.hs
+++ plutarch-unit/src/Plutarch/Test/Script.hs
@@ -38,7 +38,7 @@ import Test.Tasty.Providers (
 
 --------------------------------------------------------------------------------
 
-import Plutarch.Evaluate (evalScript, evalScriptHuge)
+import Plutarch.Evaluate (evalScriptHuge)
 import Plutarch.Script (Script)
 import Test.Tasty (testGroup)
 
@@ -149,7 +149,7 @@ runScript script debug onSuccess = case scriptResult of
   (Right _, _, _) -> (ScriptSuccess, onSuccess)
   (Left err, _, _) -> (ScriptFailure, showError dTrace (show err))
   where
-    scriptResult = evalScript script
+    scriptResult = evalScriptHuge script
     (_, _, dTrace) = evalScriptHuge debug
 
 showError :: [Text] -> String -> String
